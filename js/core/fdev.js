e329bbcc152d3a31f6ccd56ab39f45936c07c6e2
Denis <swainet@126.com>
2012-04-17 14:35:33 +0800

    update

---
+++ js/core/fdev.js
@@ -0,0 +1,9 @@
+(function(){
+    function importJS(src){
+        document.write('<script type="text/javascript" src="' + src + '"></scr' + 'ipt>');
+    }
+    importJS('http://style.china.alibaba.com/js/lib/fdev-v4/core/jquery-1.7.2.js');
+    importJS('http://style.china.alibaba.com/js/lib/fdev-v4/core/gears.js');
+    importJS('http://style.china.alibaba.com/js/lib/fdev-v4/core/web.js');
+    importJS('http://style.china.alibaba.com/js/lib/fdev-v4/core/config.js');
+})();
